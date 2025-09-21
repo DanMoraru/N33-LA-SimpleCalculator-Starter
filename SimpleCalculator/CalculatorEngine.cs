96c6f6ff64bcd54494b8f2d660f5455abd458c2e
Saurya88 <tatinenisaurya@gmail.com>
2025-09-20 20:13:35 -0400

    Finished change 1, 2 and 3

--- SimpleCalculator/CalculatorEngine.cs
+++ SimpleCalculator/CalculatorEngine.cs
@@ -4,15 +4,37 @@ namespace SimpleCalculator
 {
     public class CalculatorEngine
     {
-        public double Calculate (string argOperation, double argFirstNumber, double argSecondNumber)
+        public double Calculate(string argOperation, double argFirstNumber, double argSecondNumber)
         {
             double result = 0;
 
-            if (argOperation == "+" || argOperation.ToLower() == "add") {
+            if (argOperation == "+" || argOperation.ToLower() == "addition")
+            {
                 result = argFirstNumber + argSecondNumber;
             }
+            else if (argOperation == "-" || argOperation.ToLower() == "subtraction")
+            {
+                result = argFirstNumber - argSecondNumber;
+            }
+            else if (argOperation == "*" || argOperation.ToLower() == "multiplication")
+            {
+                result = argFirstNumber * argSecondNumber;
+            }
+            else if (argOperation == "/" ||  argOperation.ToLower() == "division")
+            {
+                result = argFirstNumber / argSecondNumber;
 
-            return result;
+                if (argSecondNumber == 0)
+                {
+                    throw new DivideByZeroException("Cannot divide by zero");
+                }
+            }
+            else
+            {
+                throw new ArgumentException("Invalid operation entered");
+            }
+
+                return result;
         }
     }
 }
--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Web;
 
 namespace SimpleCalculator
 {
@@ -14,13 +15,99 @@ namespace SimpleCalculator
                 // Class to perform actual calculations
                 CalculatorEngine calculatorEngine = new CalculatorEngine();
 
-                double firstNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
-                double secondNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
-                string operation = Console.ReadLine();
+                //double firstNumber = InputConverter.ConvertInputToNumeric(Console.ReadLine());
+                //double secondNumber = InputConverter.ConvertInputToNumeric(Console.ReadLine());
+
+
+                double firstNumber;
+                while (true)
+                {
+                    Console.WriteLine("Enter the first number: ");
+                    string num1 = Console.ReadLine();
+
+                    try
+                    {
+                        firstNumber = InputConverter.ConvertInputToNumeric(num1);
+                        break;
+                    } catch
+                    {
+                        Console.WriteLine("Invalid input. Enter a number\n");
+                    }
+                }
+
+                double secondNumber;
+                while(true)
+                {
+                    Console.WriteLine("Enter the second number: ");
+                    string num2 = Console.ReadLine();
+
+                    try
+                    {
+                        secondNumber = InputConverter.ConvertInputToNumeric(num2);
+                        break;
+
+                    } catch
+                    {
+                        Console.WriteLine("Invalid input. Enter a number\n");
+                    }
+                }
+
+                // Check if the operation is valid
+                string operation;
+                while (true)
+                {
+                    Console.WriteLine("Enter the operator: ");
+                    string input = Console.ReadLine().ToLower();
+
+                    if (input == "+" || input == "addition" || input == "-" || input == "subtraction"
+                        || input == "*" || input == "multiplication" || input == "/" || input == "division")
+                    {
+                        operation = input;
+                        break;
+
+                    } else
+                    {
+                        Console.WriteLine("Invalid Input");
+                        Console.WriteLine("\nValid inputs include: \n" +
+                            "'+', '-', '*', '/', 'addition', 'subtraction', 'multiplication', 'division'\n");
+                    }
+                }
 
                 double result = calculatorEngine.Calculate(operation, firstNumber, secondNumber);
 
-                Console.WriteLine(result);
+                // Human readable format
+                string operatorWord;
+                string op = operation.ToLower();
+
+                switch (op)
+                {
+                    case "+":
+                    case "add":
+                        operatorWord = "plus";
+                        break;
+
+                    case "-":
+                    case "subtract":
+                        operatorWord = "minus";
+                        break;
+
+                    case "*":
+                    case "multiply":
+                        operatorWord = "times";
+                        break;
+
+                    case "/":
+                    case "divide":
+                        operatorWord = "divided by";
+                        break;
+
+                    default:
+                        operatorWord = "";
+                        break;
+                }
+
+                Console.WriteLine($"\nThe value {firstNumber} {operatorWord} the value " +
+                    $"{secondNumber} is equal to {result:F2}");
 
             } catch (Exception ex)
             {
